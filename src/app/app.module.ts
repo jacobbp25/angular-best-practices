706bac96f9410483a04c5d2477ebae566f41ce75
Jacob Feuerbach <jacob.feuerbach@gmail.com>
2018-10-19 14:27:34 -0500

    Created feature module for catalog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,25 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { appRoutes } from './routes';
 import { AppComponent } from './app.component';
-import { CatalogComponent } from './catalog/catalog.component';
 import { RegisterComponent } from './users/register.component';
 import { SignInComponent } from './users/sign-in.component';
-import { CatalogRepositoryService } from './catalog/catalog-repository.service';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { CatalogModule } from './catalog/catalog.module';
 
 @NgModule({
-  imports: [BrowserModule, HttpModule, FormsModule, ReactiveFormsModule, CoreModule, SharedModule, RouterModule.forRoot(appRoutes)],
-  declarations: [AppComponent, CatalogComponent, RegisterComponent, SignInComponent],
-  providers: [CatalogRepositoryService],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CoreModule,
+    SharedModule,
+    CatalogModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  declarations: [AppComponent, RegisterComponent, SignInComponent],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
