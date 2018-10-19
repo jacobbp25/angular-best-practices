01824ede61dfa0d55a0d5d1ef998fe9c3195bd8b
Jacob Feuerbach <jacob.feuerbach@gmail.com>
2018-10-19 14:44:03 -0500

    Decorate Input and Output variables

--- src/app/shared/loading-spinner.component.ts
+++ src/app/shared/loading-spinner.component.ts
@@ -1,12 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  // tslint:disable-next-line:component-selector
   selector: 'wb-loading-spinner',
-  template: '<img *ngIf="loading" src="../../assets/images/loading.gif" />',
-  // tslint:disable-next-line:use-input-property-decorator
-  inputs: ['loading']
+  template: '<img *ngIf="loading" src="../../assets/images/loading.gif" />'
 })
 export class LoadingSpinnerComponent {
+  @Input()
   loading: boolean;
 }
