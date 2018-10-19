211e623ba847c94427f10f76ad744abf1f1fa4ad
Jacob Feuerbach <jacob.feuerbach@gmail.com>
2018-10-19 14:14:18 -0500

    Created Core Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,27 +6,17 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { appRoutes } from './routes';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { RegisterComponent } from './users/register.component';
 import { SignInComponent } from './users/sign-in.component';
 import { LoadingSpinnerComponent } from './components/loading-spinner.component';
-import { UserRepositoryService } from './services/user-repository.service';
 import { CatalogRepositoryService } from './catalog/catalog-repository.service';
-import { AccountMenuComponent } from './account-menu.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
-  imports: [BrowserModule, HttpModule, FormsModule, ReactiveFormsModule, RouterModule.forRoot(appRoutes)],
-  declarations: [
-    AppComponent,
-    NavBarComponent,
-    CatalogComponent,
-    RegisterComponent,
-    SignInComponent,
-    LoadingSpinnerComponent,
-    AccountMenuComponent
-  ],
-  providers: [UserRepositoryService, CatalogRepositoryService],
+  imports: [BrowserModule, HttpModule, FormsModule, ReactiveFormsModule, RouterModule.forRoot(appRoutes), CoreModule],
+  declarations: [AppComponent, CatalogComponent, RegisterComponent, SignInComponent, LoadingSpinnerComponent],
+  providers: [CatalogRepositoryService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
